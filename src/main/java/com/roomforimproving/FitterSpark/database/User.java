d01d2d09ecd350afd1d7d794ab227864e93d0efc
David Sudia <dsudia@gmail.com>
2016-05-04 10:09:32 -0600

    add beginning files for mongoDB

---
+++ src/main/java/com/roomforimproving/FitterSpark/database/User.java
@@ -0,0 +1,60 @@
+package com.roomforimproving.FitterSpark.database;
+
+/**
+ * Created by davidsudia on 5/4/16.
+ */
+public class User {
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String apiKey;
+
+    public User(String email, String firstname, String lastname, String password, String apiKey) {
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.apiKey = apiKey;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+}
---
+++ src/main/java/com/roomforimproving/FitterSpark/database/Users.java
@@ -0,0 +1,7 @@
+package com.roomforimproving.FitterSpark.database;
+
+/**
+ * Created by davidsudia on 5/4/16.
+ */
+public class Users {
+}
